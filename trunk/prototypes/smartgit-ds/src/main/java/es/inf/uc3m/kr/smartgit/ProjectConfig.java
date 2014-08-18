93495c544c320b18e10d66266cb6a3633cf60243
chemaar <chema.ar@gmail.com>
2014-08-18 14:48:33 +0200

    Externalizing strings

--- trunk/prototypes/smartgit-ds/src/main/java/es/inf/uc3m/kr/smartgit/ProjectConfig.java
+++ trunk/prototypes/smartgit-ds/src/main/java/es/inf/uc3m/kr/smartgit/ProjectConfig.java
@@ -11,16 +11,14 @@ public class ProjectConfig {
 
 	protected static Logger logger = Logger.getLogger(ProjectConfig.class);
 	// Change this to your current project ID
-	protected static final String PROJECT_NUMBER = "tribal-primacy-676";
-	protected static final String PROJECT_ID = "tribal-primacy-676";
-
+	protected static final String PROJECT_NUMBER = ConfigVariables.getString("ProjectConfig.PROJECT_NUMBER"); 
+	protected static final String PROJECT_ID = ConfigVariables.getString("ProjectConfig.PROJECT_ID"); 
 	// Load Client ID/secret from client_secrets.json file.
-	protected static final String CLIENTSECRETS_LOCATION = "/client_secrets.json";
+	protected static final String CLIENTSECRETS_LOCATION = ConfigVariables.getString("ProjectConfig.CLIENTSECRETS_LOCATION"); 
 	protected static GoogleClientSecrets clientSecrets = loadClientSecrets();
-
-	protected static final String TOKEN_PROPERTIES = "token.properties";
-	public static final String REFRESH_TOKEN = "refreshtoken";
-	protected static final String REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+	protected static final String TOKEN_PROPERTIES = ConfigVariables.getString("ProjectConfig.TOKEN_PROPERTIES");
+	public static final String REFRESH_TOKEN = ConfigVariables.getString("ProjectConfig.REFRESH_TOKEN"); 
+	protected static final String REDIRECT_URI = ConfigVariables.getString("ProjectConfig.REDIRECT_URI");
 
 
 	/**
@@ -34,7 +32,7 @@ public class ProjectConfig {
 							new InputStreamReader(BigQueryInstalledAuthDemo.class.getResourceAsStream(CLIENTSECRETS_LOCATION)));
 			return clientSecrets;
 		} catch (Exception e)  {
-			logger.error("Could not load clientsecrets.json");
+			logger.error("Could not load clientsecrets.json"); //$NON-NLS-1$
 			e.printStackTrace();
 		}
 		return clientSecrets;
