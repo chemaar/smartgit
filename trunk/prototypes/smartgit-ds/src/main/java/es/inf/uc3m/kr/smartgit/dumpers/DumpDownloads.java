c47c0b9cea78535bfc2ab86216943e304ec830bf
chemaar <chema.ar@gmail.com>
2014-08-20 10:47:25 +0200

    Download dumper

---
+++ trunk/prototypes/smartgit-ds/src/main/java/es/inf/uc3m/kr/smartgit/dumpers/DumpDownloads.java
@@ -0,0 +1,93 @@
+package es.inf.uc3m.kr.smartgit.dumpers;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.egit.github.core.Download;
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.Repository;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.eclipse.egit.github.core.service.DownloadService;
+import org.eclipse.egit.github.core.service.GitHubService;
+import org.eclipse.egit.github.core.service.RepositoryService;
+
+import es.inf.uc3m.kr.smartgit.DumperSerializer;
+import es.inf.uc3m.kr.smartgit.GithubConnectionHelper;
+
+public class DumpDownloads implements GitHubDumper {
+	DownloadService service;
+
+	public DumpDownloads(){
+	}
+
+
+	
+	public List<Map<Enum,String>> createDump(Map<String, Object> params) throws IOException{
+		List<Map<Enum,String>> csvData = new LinkedList<>();
+		IRepositoryIdProvider repo = (IRepositoryIdProvider) params.get(REPO_CONSTANT_PARAM);
+		List<Download> downloads = ((DownloadService) getService()).getDownloads(repo);
+		long repoID = ((Repository)repo).getId();
+		for(Download download: downloads){
+			//In case of needing memory, directly write here to a file...
+			csvData.add(describe(download,repoID));
+		}
+		return csvData;
+	}
+
+	private Map<Enum,String> describe(Download download, long id) {
+		Map<Enum,String> values = new HashMap<Enum,String>();
+		values.put(DownloadFields.ID_Repo,""+id); 
+		values.put(DownloadFields.ID,""+download.getId());
+		values.put(DownloadFields.Name,download.getName());
+		values.put(DownloadFields.Description,download.getDescription());
+		values.put(DownloadFields.URL,download.getUrl());
+		values.put(DownloadFields.Content_Type,download.getContentType());
+		values.put(DownloadFields.Count,""+download.getDownloadCount());
+		values.put(DownloadFields.HTML_URL,download.getHtmlUrl());
+		values.put(DownloadFields.Size,""+download.getSize());
+		
+
+		return values;
+	}
+
+	
+	@Override
+	public GitHubService getService() {
+		if(this.service == null){
+			this.service = new DownloadService(GithubConnectionHelper.createConnection());
+		}
+		return this.service;
+	}
+
+	@Override
+	public Enum[] getFields() {
+		return CommitFields.values();
+	}
+
+	
+	public static void main(String []args) throws IOException{
+		String DUMP_FILE="downloads-dump";
+		DumpRepository dumpRepository = new DumpRepository();
+		RepositoryService repositoryService = (RepositoryService) dumpRepository.getService();
+		GitHubDumper dumper = new DumpDownloads();
+		Map<String, Object> params = new HashMap<String,Object>();
+		for(Repository repo:repositoryService.getRepositories()){
+			System.out.println("Processing "+repo.getId());
+			params.put(REPO_CONSTANT_PARAM,repo);
+			DumperSerializer.serialize(dumper, DUMP_FILE+"-"+repo.getId()+".txt",params);
+			params.clear();
+		}
+	
+
+	}
+
+	@Override
+	public List<Map<Enum, String>> createDump() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
