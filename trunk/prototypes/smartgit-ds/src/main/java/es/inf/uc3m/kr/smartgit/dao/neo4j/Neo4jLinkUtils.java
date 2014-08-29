fa236f2d042b66be9a4a29fdf26f8ae618bc89c4
chemaar <chema.ar@gmail.com>
2014-08-29 12:36:14 +0200

    Removing unused import

--- trunk/prototypes/smartgit-ds/src/main/java/es/inf/uc3m/kr/smartgit/dao/neo4j/Neo4jLinkUtils.java
+++ trunk/prototypes/smartgit-ds/src/main/java/es/inf/uc3m/kr/smartgit/dao/neo4j/Neo4jLinkUtils.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
-import org.eclipse.egit.github.core.util.MilestoneComparator;
 import org.neo4j.cypher.javacompat.ExecutionEngine;
 import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.DynamicLabel;
